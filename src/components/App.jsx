9b70aec47df24ded409594abd35cc32d02d481ec
IrynaKniazieva <gleb0812@gmail.com>
2022-10-26 23:17:49 +0300

    in process

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,58 @@
 import React from 'react';
-import Statistics from 'components/Statistics/Statistics'
 
-const App = () =>
-  (
-    <>
-    <div>
-    <Statistics/>
-    </div>
-    </>
-  );
+
+class App extends React.Component {
+  state = {
+      good: 0,
+      neutral: 0,
+      bad: 0
+  }
+
+  handleGood = () => { 
+      this.setState(prevState => ({
+          good: prevState.good + 1,
+      }));
+  };
+
+  handleNeutral = () => { 
+      this.setState(prevState => ({
+          neutral: prevState.neutral + 1,
+      }));
+  };
+
+  handleBad = () => { 
+      this.setState(prevState => ({
+          bad: prevState.bad + 1,
+      }));
+  };
+
+  // countTotalFeedback = () => {
+  //     this.setState(prevState => ({
+  //       total: prevState.good + prevState.neutral + prevState.bad,
+  //     }))
+  // }
+
+  render () {
+      return <div>
+      <h1>Please leave feedback</h1>
+      <div>
+          <button type="button" onClick={this.handleGood}>Good</button>
+          <button type="button" onClick={this.handleNeutral}>Neutral</button>
+          <button type="button" onClick={this.handleBad}>Bad</button>
+      </div>
+      <div>
+          <h2>Statistics</h2>
+          <ul>
+              <li>Good: {this.state.good}</li>
+              <li>Neutral: {this.state.neutral}</li>
+              <li>Bad: {this.state.bad}</li>
+              <li>Total: {}</li>
+              <li>Positive feedback:</li>
+          </ul>
+      </div>
+  </div>
+  }
+}
 
 
 export default App;
